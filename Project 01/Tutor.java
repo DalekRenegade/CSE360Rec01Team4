06439971ab71e479cfa910f09e7b59df3d65747d
gchen65 <pepperguy3@yahoo.com>
2017-09-09 19:11:22 -0700

    Added comments and description

--- Project 01/Tutor.java
+++ Project 01/Tutor.java
@@ -1,52 +1,58 @@
+/**
+ * Displays HTML files
+ * Assignment number: Recitation Project 1
+ * Completion time: 11
+ *
+ * @author Gary Chen
+ * @version latest
+ */
 import javax.swing.*;
-import javax.swing.border.Border;
-
 import java.awt.*;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.io.*;
-import java.net.*;
+
 
 public class Tutor extends JPanel{
-	JLabel nameLabel;
-	String name = "GARY";
-	JEditorPane jep;
-	JScrollPane scrollPane;
+    JLabel nameLabel;
+    String name = "GARY";
+    JEditorPane jep;
+    JScrollPane scrollPane;
 
     public Tutor() {
-    	this.setLayout(new BorderLayout());
-    	nameLabel = new JLabel(name);
-    	jep = new JEditorPane();
+        this.setLayout(new BorderLayout());
+        nameLabel = new JLabel(name, SwingConstants.CENTER);
+        jep = new JEditorPane();
         jep.setEditable(false);
         jep.setContentType("text/html");
         scrollPane = new JScrollPane(jep);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         this.add(nameLabel);
-        this.addComponentListener(new ComponentAdapter() {
-			public void componentResized(ComponentEvent e) {
-				nameLabel.setFont(nameLabel.getFont().deriveFont((e.getComponent().getWidth() + e.getComponent().getHeight())/15f));
-			}
-		});
+        this.addComponentListener(new ComponentAdapter() {                          //Adjust size of text depending on JFrame size
+            public void componentResized(ComponentEvent e) {
+                nameLabel.setFont(nameLabel.getFont().deriveFont((e.getComponent().getWidth() + e.getComponent().getHeight())/15f));
+            }
+        });
     }
 
     public void changeState(int state) {
-    	if(state == 0) {
-    		this.remove(scrollPane);
-    		this.add(nameLabel);
-    	}
-    	else {
-    		this.remove(nameLabel);
-    		this.add(scrollPane);
-    		String dir = System.getProperty("user.dir");
-    		try {
-    			File file = new File("resources//P" + state + ".html");
+        if(state == 0) {
+            this.remove(scrollPane);                                                //Removes scrollPane
+            this.add(nameLabel);                                                    //Adds name to be displayed
+        }
+        else {
+            this.remove(nameLabel);                                                 //Removes name
+            this.add(scrollPane);                                                   //Adds scrollPane to be displayed
+            String dir = System.getProperty("user.dir");
+            try {
+                File file = new File("resources//P" + state + ".html");   //File path
                 jep.setPage(file.toURI().toURL());
-            } catch (IOException e) {
+            } catch (IOException e) {                                               //When file is not found or failed to load
                 jep.setText("<html>Could not load</html>");
                 System.err.println(e);
             }
-    	}
+        }
     }
 }
 
