973f02e57bfc312d89ad692b21e6747615c82297
Amit Ranjan <aranjan8@asu.edu>
2017-09-11 20:22:48 -0700

    re-added state in class and resized image on frame

--- Project 01/Companion.java
+++ Project 01/Companion.java
@@ -1,20 +1,24 @@
+/**
+ * Universe.java
+ * Recitation Project # 01
+ * Recitation Group #01 Team #04 
+ * @author Tyler Wong
+ */
+package edu.asu.CSE360._01._04;
 import java.awt.*;
-import java.awt.event.ComponentAdapter;
-import java.awt.event.ComponentEvent;
-
+import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.border.*;
 
 public class Companion extends JPanel
 {
 	String name = "Tyler";
 	private JLabel lbl;
-	
+	int state = 0;
 	private ImageIcon[] images;
 	
 	public Companion() 
-	{		
+	{
 		this.setLayout(new BorderLayout());
 		this.setBorder(BorderFactory.createCompoundBorder(new EmptyBorder(10, 10, 10, 10), new LineBorder(Color.BLACK, 3)));
 		lbl = new JLabel(name, SwingConstants.CENTER);
@@ -31,18 +35,22 @@ public class Companion extends JPanel
 		this.add(lbl);
 		this.addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
-				lbl.setFont(lbl.getFont().deriveFont((e.getComponent().getWidth() + e.getComponent().getHeight())/15f));
+				if(state > 0 && images[state - 1] != null)
+					lbl.setIcon(new ImageIcon(getScaledImage(e.getComponent().getWidth(), e.getComponent().getHeight())));
+				else
+					lbl.setFont(lbl.getFont().deriveFont((e.getComponent().getWidth() + e.getComponent().getHeight())/15f));
 			}
 		});
 	}
 	
 	public void changeState(int state)
 	{
+		this.state = state;
 		lbl.setIcon(null);
 		lbl.setText(null);
 		
 		if(state > 0) {
-			ImageIcon imgIcon = new ImageIcon(getScaledImage(this.getWidth(), this.getHeight(), state));
+			ImageIcon imgIcon = new ImageIcon(getScaledImage(this.getWidth(), this.getHeight()));
 			if(imgIcon != null && imgIcon.getIconWidth() > 0)
 				lbl.setIcon(imgIcon);
 			else
@@ -53,7 +61,7 @@ public class Companion extends JPanel
 		this.revalidate();
 	}
 	
-	private Image getScaledImage(int boundaryWidth, int boundaryHeight, int state) {
+	private Image getScaledImage(int boundaryWidth, int boundaryHeight) {
 		ImageIcon imgIcon = images[state - 1];
 		Image scaledImg = null;
 		int newWidth = imgIcon.getIconWidth();
